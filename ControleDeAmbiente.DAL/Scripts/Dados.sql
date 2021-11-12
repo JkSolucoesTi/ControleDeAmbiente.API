2025a1d9cd0fac944a5d3c576ab868e33b60a1d8
jksolucoesti <jksolucoesti@gmail.com>
2021-11-12 16:34:41 -0300

    Scripts

---
+++ ControleDeAmbiente.DAL/Scripts/Dados.sql
@@ -0,0 +1,32 @@
+use controledeambiente
+go
+
+BEGIN TRAN
+BEGIN TRY
+
+insert into android (Nome)
+values('Sem alocação'),('Adauto'),('Valeria'),('Romulo')
+
+insert into api (Nome)
+values('Sem alocação'),('Ciro'),('Marco'),('Michele'),('Marcelo'),('Douglas'),('Carlos')
+
+insert into ios (Nome)
+values('Sem alocação'),('Jarber'),('Will'),('Andre')
+
+insert into ambientes(Nome,Chamado,Descricao,ApiId,IosId,AndroidId)
+values('DEV 01','','Ambiente Disponível',1,1,1),
+	  ('DEV 02','','Ambiente Disponível',1,1,1),
+	  ('DEV 03','','Ambiente Disponível',1,1,1),
+	  ('DEV 04','','Ambiente Disponível',1,1,1),
+	  ('DEV 05','','Ambiente Disponível',1,1,1),
+	  ('DEV 06','','Ambiente Disponível',1,1,1),
+	  ('DEV 07','','Ambiente Disponível',1,1,1),
+	  ('DEV 08','','Ambiente Disponível',1,1,1),
+	  ('DEV 09','','Ambiente Disponível',1,1,1),
+	  ('DEV 10','','Ambiente Disponível',1,1,1)
+
+COMMIT TRAN
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+END CATCH
